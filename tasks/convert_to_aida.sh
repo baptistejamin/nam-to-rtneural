ed9b68b68b97e5123035e87f5640c453c3f63f1e
Baptiste Jamin <baptiste@crisp.chat>
2024-10-03 21:55:10 +0200

    Less epoch

--- tasks/convert_to_aida.sh
+++ tasks/convert_to_aida.sh
@@ -6,4 +6,4 @@ rm -r -f Results/amp_*
 
 python3 prep_wav.py -f ../../dataset/input.wav  ../../dataset/output.wav -l LSTM-16.json -n
 
-python3 dist_model_recnet.py -l LSTM-16.json -slen 24000 --seed 39 -lm 0
+python3 dist_model_recnet.py -l LSTM-16.json -slen 24000 --epoch 200 --seed 39 -lm 0
